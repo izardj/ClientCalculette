261e105b72dfd8ae6aa7cecbd2a546c3d506e251
izardj <izard.jerome@free.fr>
2017-05-02 15:16:42 +0200

    Nettoyage d'une contion action inutile

--- src/servlets/CalculServlet.java
+++ src/servlets/CalculServlet.java
@@ -18,52 +18,54 @@ import ejb.CalculatriceRemote;
 @WebServlet("/CalculServlet")
 public class CalculServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+
 	@EJB
 	private CalculatriceLocal calculatriceRemote;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CalculServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public CalculServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
-		
+
 		int x = Integer.parseInt(request.getParameter("x"));
 		int y = Integer.parseInt(request.getParameter("y"));
-		String action = request.getParameter("action");
-		
+
 		int resultatAdddition = 0;
 		int resultatSoustraction = 0;
 		int resultatDivision = 0;
 		int resultatMultiplication = 0;
-		
-		if(action.equals("ajouter")){
-			resultatAdddition = calculatriceRemote.addition(x, y);
-			resultatSoustraction = calculatriceRemote.soustraction(x, y);
-			resultatDivision = calculatriceRemote.division(x, y);
-			resultatMultiplication = calculatriceRemote.multiplication(x, y);
-			request.setAttribute("resultatAddition", resultatAdddition);
-			request.setAttribute("resultatSoustraction", resultatSoustraction);
-			request.setAttribute("resultatDivision", resultatDivision);
-			request.setAttribute("resultatMultiplication", resultatMultiplication);
-			request.setAttribute("action", action);
-			request.getRequestDispatcher("index.jsp").forward(request, response);;
-		}
+
+		resultatAdddition = calculatriceRemote.addition(x, y);
+		resultatSoustraction = calculatriceRemote.soustraction(x, y);
+		resultatDivision = calculatriceRemote.division(x, y);
+		resultatMultiplication = calculatriceRemote.multiplication(x, y);
+		request.setAttribute("resultatAddition", resultatAdddition);
+		request.setAttribute("resultatSoustraction", resultatSoustraction);
+		request.setAttribute("resultatDivision", resultatDivision);
+		request.setAttribute("resultatMultiplication", resultatMultiplication);
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		;
 
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
